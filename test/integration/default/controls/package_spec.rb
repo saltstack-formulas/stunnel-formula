3738a21f892184fc081d3c6429d0418079dee616
Imran Iqbal <iqbalmy@hotmail.com>
2019-10-12 01:30:31 +0100

    fix(rubocop): add fixes using `rubocop --safe-auto-correct`

--- test/integration/default/controls/package_spec.rb
+++ test/integration/default/controls/package_spec.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 control 'Stunnel package' do
   title 'should be installed'
-
 
   describe package('stunnel4') do
     it { should be_installed }
--- test/integration/default/controls/service_spec.rb
+++ test/integration/default/controls/service_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 control 'Stunnel service' do
   title 'should be running and enabled'
-
 
   describe service('stunnel4') do
     it { should be_enabled }
     it { should be_running }
   end
 
-  describe port(12003) do
+  describe port(12_003) do
     it { should be_listening }
   end
 end
